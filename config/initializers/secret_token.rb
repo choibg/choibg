bf2b63afd118d3c3fd36d90b0717b06bc15dcacf
your name <you@exampl.com>
2013-06-10 18:47:03 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Choibg::Application.config.secret_token = 'dc0160104268e4d7f736e4cc8e7d7f7f81d80aecbe8274a5359df8846753f6332f9fc74e993d2eae9c76b0bcc0546a8edb54e8a01b59aeed828ff0fe2d2d074f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Choibg::Application.config.session_store :cookie_store, key: '_choibg_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Choibg::Application.config.session_store :active_record_store
